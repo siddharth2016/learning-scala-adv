ea72b9c9116fc33f32d12631544618363c7e209f
siddharth2016 <siddharthchandragzb@gmail.com>
2021-05-19 22:46:13 +0530

    update files

--- src/lectures/part1afp/PartialFunctions.scala
+++ src/lectures/part1afp/PartialFunctions.scala
@@ -1,5 +1,50 @@
 package lectures.part1afp
 
 object PartialFunctions extends App {
-  print("Inside Partial Functions")
+  println("Inside Partial Functions")
+
+  val aFussyFuntion: PartialFunction[Int, Int] = {
+    case 1 => 1
+    case 2 => 2
+    case 5 => 5
+  }
+
+  println(aFussyFuntion(5))
+
+  println(aFussyFuntion.isDefinedAt(3))
+
+  val lift = aFussyFuntion.lift
+  println(lift(10))
+
+  val newFussyFunction = aFussyFuntion.orElse[Int, Int] {
+    case 10 => 10
+  }
+
+  println(newFussyFunction(10))
+
+  // Anonymous Partial Function
+
+  val aPartialFunction = new PartialFunction[Int, Int] {
+    override def apply(v1: Int): Int = v1 match {
+      case 1 => 1
+      case 2 => 2
+      case 3 => 3
+    }
+
+    override def isDefinedAt(x: Int): Boolean =
+      x == 1 || x == 2 || x == 3
+  }
+
+  println(aPartialFunction(1))
+
+  // Dumb Chatbot
+
+  val chatBot: PartialFunction[String, String] = {
+    case "Hello" => "Hi"
+    case "Tell me something nice" => "Have a nice day"
+    case "Bye" => "Bye, See you"
+    case _ => "Sorry, I am not trained on this data!"
+  }
+
+  scala.io.Source.stdin.getLines().foreach(x => println(chatBot(x)))
 }
