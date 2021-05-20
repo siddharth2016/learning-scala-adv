780904bed8aa46bd059cedfacf611f50b4d1384c
siddharth2016 <siddharthchandragzb@gmail.com>
2021-05-20 17:43:57 +0530

    update files

--- src/lectures/part1afp/LazyEvaluation.scala
+++ src/lectures/part1afp/LazyEvaluation.scala
@@ -39,5 +39,33 @@ object LazyEvaluation extends App {
 
   println(byNameMethod(threadMethod))
 
+  // withFilter
 
+  def lessThan30(x: Int): Boolean = {
+    println(s"$x is less than 30?")
+    x < 30
+  }
+
+  def greaterThan20(x: Int): Boolean = {
+    println(s"$x is greater than 20?")
+    x > 20
+  }
+
+  val numbers = List(1, 2, 25, 40, 23, 5)
+  val ls30 = numbers.filter(lessThan30)
+  val gt20 = ls30.filter(greaterThan20)
+  println(gt20)
+
+  val ls30lazy = numbers.withFilter(lessThan30)
+  val gt20lazy = ls30lazy.withFilter(greaterThan20)
+  println()
+  gt20lazy.foreach(println)
+
+  // for-comprehension with an if guard uses withFilter
+  val even = for {
+    n <- List(1,2,3) if n % 2 == 0
+  } yield n
+  // List(1,2,3).withFilter(_ % 2 == 0).map(_)
+  println(even)
+  println(List(1,2,3).withFilter(_ % 2 == 0).map(x => x))
 }
