0d57fb90150c33c8db317d70d1272f8250dc9f18
siddharth2016 <siddharthchandragzb@gmail.com>
2021-05-24 12:27:24 +0530

    update files

--- src/lectures/part3concurrency/Intro.scala
+++ src/lectures/part3concurrency/Intro.scala
@@ -1,5 +1,38 @@
 package lectures.part3concurrency
 
+import java.util.concurrent.{Executor, Executors}
+
 object Intro extends App {
-  print("Hello JVM Concurrency Intro")
+  println("Hello JVM Concurrency Intro")
+
+  val aThread = new Thread(new Runnable {
+    override def run(): Unit = println("Inside Runnable")
+  })
+
+  aThread.start()
+  aThread.join()
+
+  val threadHello = new Thread(() => (1 to 5).foreach(_ => println("Hello")))
+  val threadBye = new Thread(() => (1 to 5).foreach(_ => println("Bye")))
+
+  threadHello.start()
+  threadBye.start()
+  threadHello.join()
+  threadBye.join()
+
+  val pool = Executors.newFixedThreadPool(10)
+  pool.execute(() => println("inside pool executor"))
+  pool.execute(() => {
+    Thread.sleep(1000)
+    println("done after 1 sec")
+  })
+  pool.execute(() => {
+    Thread.sleep(1000)
+    println("almost done")
+    Thread.sleep(1000)
+    println("done after 2 sec")
+  })
+
+  pool.shutdown()
+  println(pool.isShutdown)
 }
