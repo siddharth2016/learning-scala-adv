36599b7a8814b2e44a5ff4ae3ffcf906b15d134f
siddharth2016 <siddharthchandragzb@gmail.com>
2021-07-02 12:29:32 +0530

    update files

--- src/lectures/part4implicits/ImplicitsIntro.scala
+++ src/lectures/part4implicits/ImplicitsIntro.scala
@@ -2,4 +2,21 @@ package lectures.part4implicits
 
 object ImplicitsIntro extends App {
 
+  case class Person(name: String) {
+    def greet = s"Hi, $name"
+  }
+
+  implicit def createPerson(str: String): Person = Person(str)
+
+  println("Sid")  // no implicit conversion
+  println("Sid".greet)
+
+  def sum(x: Int)(implicit amt: Int) = x + amt
+
+//  implicit val defAmt: Int = 10
+  implicit val y: Int = 11
+
+  println(sum(2)(2))  // no implicit conversion
+  println(sum(2))
+
 }
